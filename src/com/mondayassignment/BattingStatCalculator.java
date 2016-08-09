b8f1f2f755bbf4e04c0f63b45d5493dc6ac4a056
rocheeli <bethsroche@gmail.com>
2016-08-09 15:57:10 -0400

    added exception 8/9/16

--- src/com/mondayassignment/BattingStatCalculator.java
+++ src/com/mondayassignment/BattingStatCalculator.java
@@ -3,28 +3,53 @@ package com.mondayassignment;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class BattingStatCalculator {
+public class BattingStatCalculator 
+{
+	
+
 public static void main(String[] args)
 {
-	Scanner bats = new Scanner(System.in);
-	Scanner bases = new Scanner(System.in);
+		
+	
+	
 	
 	float  sumOfBasesFromAtBats = 0;
 	float battingAverage = 0;
 	float sluggingPercentage = 0;
 	float totalHits = 0;
 	
+	ArrayList<Float> atBatsResults = new ArrayList <>();
 	System.out.println("Welcome to the Batting Stat Calculator!");
 	System.out.println("Enter the number of at-bats for the player:");
+	Scanner bats = new Scanner(System.in);
 	String atBats = bats.nextLine();
+	
+	try
+	{
+		int testNum = Integer.parseInt(atBats);
+		if (Float.parseFloat(atBats) < 0)
+		{
+			throw new Exception ("lessThanZero");
+		}
+		else
+	{
+			Scanner bases = new Scanner(System.in);	
 	float numberOfAtBatsAsFloat = Float.parseFloat(atBats);
-	ArrayList<Float> atBatsResults = new ArrayList <>();
 
 	
 	for (int i = 0; i < numberOfAtBatsAsFloat; i++) 
 	{
 		System.out.println("What was the result of at bat "  + (i + 1) +" (valid answers are 0-4)");
 		String results = bats.nextLine();
+		int testNum2 = Integer.parseInt(results);
+		if (Float.parseFloat(results) > 4)
+		{
+			throw new Exception("greaterThanFour");
+		}
+		if (Float.parseFloat(results) < 0)
+		{
+			throw new Exception("lessThanZero");
+		}
 		float numberOfAtBats = Float.parseFloat(results);
 		
 		atBatsResults.add(numberOfAtBats);
@@ -53,12 +78,17 @@ public static void main(String[] args)
 	battingAverage = totalHits / numberOfAtBatsAsFloat;
 	System.out.println();
 	System.out.printf("The player's batting average is %.3f" ,  battingAverage);
-	
-		}
-  
 	}
+	}	
+	catch (Exception Negentry)
+	{
+		System.out.println("You made an error. Please try again");
+	}
+	}
+}
 
 	
-	
+
+
 	
 
